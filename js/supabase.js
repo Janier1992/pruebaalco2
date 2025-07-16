bd92265335f8513666148ab123796bd3b00f3ae0
JANIER MOSQUERA MOSQUERA <117685964+Janier1992@users.noreply.github.com>
2025-07-15 22:46:24 -0500

    Add files via upload

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+// js/supabase.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+const supabaseUrl = 'https://nnpxhhjtnbidjqvhpsni.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ucHhoaGp0bmJpZGpxdmhwc25pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzNDgyMDMsImV4cCI6MjA2NzkyNDIwM30.stXP4l6OX4-AymYQMIR2ncxnX_nDN7puQicOxVjMu-Q'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
